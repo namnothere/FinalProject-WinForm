67b01c13e8ad9ed66e427cc900b90003bbf6b8c8
FusernameC <88580614+FusernameC@users.noreply.github.com>
2022-06-08 23:20:36 +0700

    -Fix conflict STAFF

--- HotelManagement/Employee Management/STAFF.cs
+++ HotelManagement/Employee Management/STAFF.cs
@@ -54,19 +54,25 @@ namespace hotel_management
         }
 
 
-        public bool insertEmployee(string staff_id, string staff_name, DateTime DOB, string staff_address, string staff_phone, string staff_type, string username, MemoryStream staff_img)
+        public bool insertEmployee(string staff_id, string staff_name, DateTime DOB, string staff_address, string staff_phone, string staff_sex, string staff_type, string username, MemoryStream staff_img)
         {
-            SqlCommand cmd = new SqlCommand("INSERT INTO staffs VALUES(@Id, @name, @DOB, @address, @phone, @type, @username, @img)", mydb.getConnection);
+            SqlCommand cmd = new SqlCommand("INSERT INTO staffs VALUES(@Id, @name, @DOB, @address, @phone, @sex, @type, @username, @img)", mydb.getConnection);
 
             cmd.Parameters.AddWithValue("@Id", staff_id);
             cmd.Parameters.AddWithValue("@name", staff_name);
             cmd.Parameters.AddWithValue("@DOB", DOB);
             cmd.Parameters.AddWithValue("@address", staff_address);
             cmd.Parameters.AddWithValue("@phone", staff_phone);
+            cmd.Parameters.AddWithValue("@sex", staff_sex);
             cmd.Parameters.AddWithValue("@type", staff_type);
             cmd.Parameters.AddWithValue("@img", staff_img.ToArray());
             cmd.Parameters.AddWithValue("@username", username);
+<<<<<<< HEAD
 
+=======
+            cmd.Parameters.AddWithValue("@DOB", DOB);
+            cmd.Parameters.AddWithValue("@sex", sex);
+>>>>>>> fa9d19723868403d5432931c72af4b4906e82121
             mydb.openConnection();
             if (cmd.ExecuteNonQuery() == 1)
             {
@@ -75,12 +81,25 @@ namespace hotel_management
             return false;
         }
 
-
-
-        public bool updateEmployee(string staff_id, string staff_name, DateTime DOB, string staff_address, string staff_phone, string staff_type, string username, MemoryStream staff_img)
+        public bool setUsernameID(string username)
         {
-            SqlCommand cmd = new SqlCommand("UPDATE STAFF SET staff_name = @staff_name, staff_address = @staff_address, staff_phone = @staff_phone, staff_type = @staff_type, staff_img = @staff_img WHERE staff_id = @staff_id", mydb.getConnection);
-            cmd.Parameters.AddWithValue("@Id", staff_id);
+            ACCOUNT acc = new ACCOUNT();
+            int id = acc.getID(username);
+            SqlCommand cmd = new SqlCommand("UPDATE staffs SET usernameID = @id WHERE username = @username", mydb.getConnection);
+            cmd.Parameters.AddWithValue("@id", id);
+            cmd.Parameters.AddWithValue("@username", username);
+            mydb.openConnection();
+            if (cmd.ExecuteNonQuery() == 1)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool updateEmployee(string staff_id, string staff_name, DateTime DOB, string staff_address, string staff_phone, string sex, string staff_type, string username, MemoryStream staff_img)
+        {
+            SqlCommand cmd = new SqlCommand("UPDATE staffs SET name = @name, address = @address, phone = @phone, type = @type, profilePic = @img, sex = @sex, username = @username WHERE id = @staff_id", mydb.getConnection);
+            cmd.Parameters.AddWithValue("@staff_id", staff_id);
             cmd.Parameters.AddWithValue("@name", staff_name);
             cmd.Parameters.AddWithValue("@address", staff_address);
             cmd.Parameters.AddWithValue("@phone", staff_phone);
@@ -88,6 +107,10 @@ namespace hotel_management
             cmd.Parameters.AddWithValue("@img", staff_img.ToArray());
             cmd.Parameters.AddWithValue("@username", username);
             cmd.Parameters.AddWithValue("@DOB", DOB);
+            cmd.Parameters.AddWithValue("@sex", sex);
+
+            mydb.openConnection();
+
             if (cmd.ExecuteNonQuery() == 1)
             {
                 return true;
@@ -98,7 +121,7 @@ namespace hotel_management
 
         public bool deleteEmployee(string staff_id)
         {
-            SqlCommand cmd = new SqlCommand("DELETE FROM STAFF WHERE staff_id = @staff_id", mydb.getConnection);
+            SqlCommand cmd = new SqlCommand("DELETE FROM staffs WHERE id = @staff_id", mydb.getConnection);
             cmd.Parameters.AddWithValue("@staff_id", staff_id);
             if (cmd.ExecuteNonQuery() == 1)
             {
